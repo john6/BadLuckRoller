8dbee197d7d33f59177105b8f9c008a50cd8aca9
Spencer Rawls <spencerrawls@Spencers-MacBook-Pro.local>
2019-11-08 22:57:36 -0800

    Basic player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float lookSensitivity;
+    [SerializeField] private float maxPitch;
+    [SerializeField] private float minPitch;
+    [SerializeField] private float launchSpeed;
+    [SerializeField] private float spread;
+    [SerializeField] private GameObject die;
+    [SerializeField] private Transform dieSpawnPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        float yaw = transform.eulerAngles.y;
+        yaw += x * lookSensitivity;
+
+        float pitch = transform.eulerAngles.x;
+        pitch -= Mathf.Clamp(y * lookSensitivity, minPitch, maxPitch);
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Launch();
+        }
+    }
+
+    private void Launch()
+    {
+        GameObject obj = Instantiate(die, dieSpawnPosition.position, dieSpawnPosition.rotation);
+        Rigidbody body = obj.GetComponent<Rigidbody>();
+        Vector3 velocity = transform.forward * launchSpeed;
+        body.velocity = velocity;
     }
 }
