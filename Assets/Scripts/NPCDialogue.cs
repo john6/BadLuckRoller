2ed29e5a24a651eeb55b197d14ca07f4bae6a11d
GameMakeJake <jacobshillan@gmail.com>
2019-11-10 11:20:16 -0800

    sss

---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPCDialogue: MonoBehaviour
+{
+    [SerializeField] private Text customText;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Dice"))
+        {
+            customText.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Dice"))
+        {
+            customText.enabled = false;
+        }
+    }
+
+}
