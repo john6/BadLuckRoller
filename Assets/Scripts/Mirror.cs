80f14743c8bf5ba4a68e1bc60621dcdd12bd0ff6
Spencer Rawls <spencerrawls@Spencers-MacBook-Pro.local>
2019-11-09 18:36:12 -0800

    Updated shattering on mirror

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Mirror : LuckObject
 {
-    [SerializeField] private Material unbrokenMaterial;
-    [SerializeField] private Material brokenMaterial;
+    [SerializeField] private float explosionForce;
+    [SerializeField] private float explosionRadius;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Dice"))
         {
-            Break();
+            Break(collision.contacts[0].point);
         }
     }
 
-    private void Break()
+    private void Break(Vector3 point)
     {
-        MeshRenderer renderer = GetComponent<MeshRenderer>();
-        renderer.material = brokenMaterial;
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < bodies.Length; ++i)
+        {
+            bodies[i].isKinematic = false;
+            bodies[i].AddExplosionForce(explosionForce, point, explosionRadius);
+        }
+
         AlterLuck();
     }
 }
